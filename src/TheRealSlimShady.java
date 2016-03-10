fe7c44c85da1511d07db3501bb4668b887fea70d
Michael Huynh <mhuynh@caltech.edu>
2016-03-10 02:31:38 -0800

    implemented heuristics

--- src/TheRealSlimShady.java
+++ src/TheRealSlimShady.java
@@ -1,8 +1,19 @@
 public class TheRealSlimShady implements OthelloPlayer
 {
-
     private OthelloGame game;
+
     private OthelloSide mySide;
+
+    private int[][] heuristicScore = {
+            {100, -25, 5, 5, 5, 5, -25, 100},
+            {-25, -50, 5, 5, 5, 5, -50, -25},
+            {5, 5, 5, 5, 5, 5, 5, 5},
+            {5, 5, 5, 5, 5, 5, 5, 5},
+            {5, 5, 5, 5, 5, 5, 5, 5},
+            {5, 5, 5, 5, 5, 5, 5, 5},
+            {-25, -50, 5, 5, 5, 5, -50, -25},
+            {100, -25, 5, 5, 5, 5, -25, 100}
+    };
 
     @Override
     public void init(OthelloSide side)
@@ -13,22 +24,27 @@ public class TheRealSlimShady implements OthelloPlayer
     @Override
     public Move doMove(Move opponentsMove, long millisLeft)
     {
-        int x = 0, y = 0;
+        if (!game.getBoard()
+                .hasMoves(mySide))
+        {
+            return null;
+        }
+
+        OthelloBoard b = game.getBoard();
+        int x = 0, y = 0, min_score = -100;
         Move result = new Move(x, y);
 
-        while (!game.getBoard()
-                .checkMove(new Move(x, y), mySide))
+        while (!b.checkMove(result, mySide))
         {
+            if (heuristicScore[y][x] > min_score)
+            {
+                result.set(x, y);
+            }
             if (++x > 7)
             {
                 x = 0;
                 ++y;
             }
-            if (y > 7)
-            {
-                return null;
-            }
-            result.set(x, y);
         }
 
         return result;
