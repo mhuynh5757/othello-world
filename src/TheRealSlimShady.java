4ac14a054fa2c48d770275f12c098dc63d58ad9f
Michael Huynh <mhuynh@caltech.edu>
2016-03-10 16:26:26 -0800

    made first-level non-recursive minimax

--- src/TheRealSlimShady.java
+++ src/TheRealSlimShady.java
@@ -1,8 +1,34 @@
+import java.util.ArrayList;
+
+class scorePair
+{
+    int score = 0;
+    Move move;
+
+    scorePair(int s, Move m)
+    {
+        score = s;
+        move = m;
+    }
+
+    int getScore()
+    {
+        return score;
+    }
+
+    Move getMove()
+    {
+        return move;
+    }
+}
+
 public class TheRealSlimShady implements OthelloPlayer
 {
     private OthelloGame game;
 
     private OthelloSide mySide;
+
+    private OthelloSide oppSide;
 
     private int[][] heuristicScore = {
             {100, -25, 5, 5, 5, 5, -25, 100},
@@ -19,6 +45,7 @@ public class TheRealSlimShady implements OthelloPlayer
     public void init(OthelloSide side)
     {
         mySide = side;
+        oppSide = mySide.opposite();
     }
 
     @Override
@@ -31,23 +58,89 @@ public class TheRealSlimShady implements OthelloPlayer
         }
 
         OthelloBoard b = game.getBoard();
-        int x = 0, y = 0, min_score = -100;
-        Move result = new Move(x, y);
 
-        while (!b.checkMove(result, mySide))
+        scorePair res = minimax(b);
+
+        return res.getMove();
+    }
+
+    private int scoreBoard(OthelloBoard b)
+    {
+        int score = 0;
+        for (int i = 0; i < 8; i++)
         {
-            if (heuristicScore[y][x] > min_score)
+            for (int j = 0; j < 8; j++)
             {
-                result.set(x, y);
-            }
-            if (++x > 7)
-            {
-                x = 0;
-                ++y;
+                if (b.get(mySide, j, i))
+                {
+                    score += heuristicScore[i][j];
+                }
+                else
+                {
+                    score -= heuristicScore[i][j];
+                }
             }
         }
+        return score;
+    }
 
-        return result;
+    private scorePair minimax(OthelloBoard b)
+    {
+        ArrayList<scorePair> scores = new ArrayList<>();
+
+        if (b.hasMoves(mySide))
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Move myMove = new Move(j, i);
+                    if (b.checkMove(myMove, mySide))
+                    {
+                        OthelloBoard my_new_board = b.copy();
+                        my_new_board.move(myMove, mySide);
+                        if (my_new_board.hasMoves(oppSide))
+                        {
+                            for (int k = 0; k < 8; k++)
+                            {
+                                for (int l = 0; l < 8; l++)
+                                {
+                                    Move theirMove = new Move(l, k);
+                                    if (my_new_board.checkMove(theirMove, oppSide))
+                                    {
+                                        OthelloBoard their_new_board = my_new_board.copy();
+                                        their_new_board.move(theirMove, oppSide);
+                                        scores.add(new scorePair(scoreBoard(their_new_board), myMove));
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            scores.add(new scorePair(scoreBoard(my_new_board), myMove));
+                        }
+                    }
+                }
+            }
+        }
+        if (scores.size() > 0)
+        {
+            int best_score = scores.get(0).getScore();
+            int best_sp = 0;
+            for (int i = 0; i < scores.size(); i++)
+            {
+                if (best_score < scores.get(i).getScore())
+                {
+                    best_score = scores.get(i).getScore();
+                    best_sp = i;
+                }
+            }
+            return scores.get(best_sp);
+        }
+        else
+        {
+            return null;
+        }
     }
 
     public void setGame(OthelloGame g)
